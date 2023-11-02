d8c002bdaef9ec2c2b9752086f1dd51829a5e515
mir21bek <mirbek.2121@gmail.com>
2023-11-02 21:31:56 +0600

    T2-87: improved signals.py

--- users/signals.py
+++ users/signals.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from users.models import StaffUser
 
 
-@receiver(post_save, sender=StaffUser)
+@receiver(post_save, sender=StaffUser, dispatch_uid='unique_identifier')
 def post_user_save(sender, instance, created, **kwargs):
     if created:
         print(F'Пользователь с именем {instance.username} создан')
