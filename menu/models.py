be11c4671090b1a17991c9f1fe9d0ae4b015c932
mir21bek <mirbek.2121@gmail.com>
2023-11-04 16:39:25 +0600

    T2-119: Updated models

--- menu/models.py
+++ menu/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Menu(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='media/food_image', verbose_name='Фото блюды', null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
@@ -18,4 +18,18 @@ class Menu(models.Model):
     popular = models.BooleanField(default=False, verbose_name='Популярные')
 
     def __str__(self):
+        return f"{self.category}: {self.name}"
+
+
+class ExtraItem(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название доп. продукта')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
         return self.name
+
+
+class ExtraProduct(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    extra = models.ForeignKey(ExtraItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
