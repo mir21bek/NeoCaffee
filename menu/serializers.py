4d516d7122c798bff6c3a25388d8720c16cb313c
mir21bek <mirbek.2121@gmail.com>
2023-11-04 18:56:02 +0600

    T2-120: create serializers for menu

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,64 +1,84 @@
 from rest_framework import serializers
 
-from .models import Category, Menu
+from .models import Category, Menu, ExtraItem
 
 
 class CategorySerializer(serializers.ModelSerializer):
     """Сериализатор для модели Category.
 
-    Этот сериализатор используется для преобразования объектов Category в JSON и наоборот.
+    Преобразует объекты Category в JSON и обратно.
     """
 
     class Meta:
         model = Category
-        fields = ['name']  # Поля модели Category, которые будут включены в сериализацию
+        fields = ['name']  # Поля модели Category, включаемые в сериализацию
 
     def create(self, validated_data):
-        """Метод для создания нового объекта Category.
-
-        Args:
-            validated_data (dict): Валидированные данные для создания объекта Category.
-
-        Returns:
-            Category: Созданный объект Category.
-        """
+        """Создает новый объект Category."""
         category = Category.objects.create(**validated_data)
         return category
 
     def update(self, instance, validated_data):
-        """Метод для обновления существующего объекта Category.
-
-        Args:
-            instance (Category): Существующий объект Category, который нужно обновить.
-            validated_data (dict): Валидированные данные для обновления объекта Category.
-
-        Returns:
-            Category: Обновленный объект Category.
-        """
+        """Обновляет существующий объект Category."""
         instance.name = validated_data.get('name', instance.name)
         instance.save()
-        return instance
+
+    def __delete__(self, instance):
+        """Удаляет объект Category."""
+        instance.delete()
 
 
 class MenuSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Menu.
 
-    Этот сериализатор используется для преобразования объектов Menu в JSON и наоборот.
+    Преобразует объекты Menu в JSON и обратно.
     """
+
+    category = serializers.ReadOnlyField()
 
     class Meta:
         model = Menu
         fields = ('name', 'category', 'description', 'image', 'price', 'available', 'popular')
-        # Поля модели Menu, которые будут включены в сериализацию
+        # Поля модели Menu, включаемые в сериализацию
 
     def create(self, validated_data):
-        """Метод для создания нового объекта Menu.
-
-        Args:
-            validated_data (dict): Валидированные данные для создания объекта Menu.
-
-        Returns:
-            Menu: Созданный объект Menu.
-        """
+        """Создает новый объект Menu."""
         menu = Menu.objects.create(**validated_data)
         return menu
+
+    def update(self, instance, validated_data):
+        """Обновляет существующий объект Menu."""
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.image = validated_data.get('image', instance.image)
+        instance.price = validated_data.get('price', instance.price)
+        instance.available = validated_data.get('available', instance.available)
+        instance.popular = validated_data.get('popular', instance.popular)
+        instance.save()
+
+    def __delete__(self, instance):
+        """Удаляет объект Menu."""
+        instance.delete()
+
+
+class ExtraItemSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели ExtraItem."""
+
+    class Meta:
+        model = ExtraItem
+        fields = ('name', 'price')
+
+    def create(self, validated_data):
+        """Создает новый объект ExtraItem."""
+        extra_item = ExtraItem.objects.create(**validated_data)
+        return extra_item
+
+    def update(self, instance, validated_data):
+        """Обновляет существующий объект ExtraItem."""
+        instance.name = validated_data.get('name', instance.name)
+        instance.price = validated_data.get('price', instance.price)
+        instance.save()
+
+    def __delete__(self, instance):
+        """Удаляет объект ExtraItem."""
+        instance.delete()
