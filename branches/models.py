f6bfd34da2201819afb832b49ac5d8d0729b34ae
mir21bek <mirbek.2121@gmail.com>
2023-11-06 19:44:22 +0600

    T2-105: update models

--- branches/models.py
+++ branches/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from menu.models import Menu
+from menu.models import Menu, Category
 
 
 class Branches(models.Model):
@@ -15,8 +15,12 @@ class Branches(models.Model):
 
 
 class CoffeeShop(models.Model):
-    branch = models.ForeignKey(Branches, on_delete=models.CASCADE, related_name='branches', verbose_name='Филиал')
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu', verbose_name='Меню')
+    branch = models.ForeignKey(Branches, on_delete=models.CASCADE,
+                               related_name='branches', verbose_name='Филиал')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 verbose_name='Категория', related_name='coffee_shop')
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE,
+                             related_name='menu', verbose_name='Меню')
 
     def __str__(self):
         return self.branch
