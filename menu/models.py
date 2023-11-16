a6f6e6aa43d9940e410edb84263b2b8c846f3127
mir21bek <mirbek.2121@gmail.com>
2023-11-17 04:30:26 +0600

    T2-153: Updated orders rewrite some functions

--- menu/models.py
+++ menu/models.py
@@ -15,7 +15,9 @@ class Category(models.Model):
 
 class Menu(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 null=True, verbose_name='Категория',
+                                 related_name='menus')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='media/food_image', verbose_name='Фото блюды', null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
