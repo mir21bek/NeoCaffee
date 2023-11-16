ec19ba56c9c3b842265bebe2414e2f21c3fac727
mir21bek <mirbek.2121@gmail.com>
2023-11-16 23:54:39 +0600

    T2-189: Solve some code bugs

--- customers/utils.py
+++ customers/utils.py
@@ -12,6 +12,6 @@ def send_otp(phone_number, otp):
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
     message = client.messages.create(
         body=f'Your verification code is: {otp}',
-        from_=settings.TWILIO_PHONE_NUMBER,
+        from_=settings.TWILIO_SERVER_SID,
         to=phone_number
     )
