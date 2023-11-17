0cd5b89ff71d1db66019e23dc80ec0e9d4d0cbe0
mir21bek <mirbek.2121@gmail.com>
2023-11-17 18:26:24 +0600

    T2-118: Updated models and views also create urls

--- menu/admin.py
+++ menu/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Category)
-admin.site.register(Menu)
-admin.site.register(ExtraItem)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category', 'price',
+                    'available', 'created', 'popular')
+    list_filter = ('available', 'popular', 'created', 'updated')
+    list_editable = ('price', 'popular', 'available')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(ExtraItem)
+class ExtraItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    list_filter = ('name', 'price')
--- menu/serializers.py
+++ menu/serializers.py
@@ -1,78 +1,24 @@
 from rest_framework import serializers
 
-from .models import Category, Menu, ExtraItem, ExtraProduct
+from .models import Category, Menu, ExtraItem
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Category.
-
-    Преобразует объекты Category в JSON и обратно.
-    """
 
     class Meta:
         model = Category
-        fields = ('name', 'image')  # Поля модели Category, включаемые в сериализацию
-
-    def create(self, validated_data):
-        """Создает новый объект Category."""
-        category = Category.objects.create(**validated_data)
-        return category
-
-    def update(self, instance, validated_data):
-        """Обновляет существующий объект Category."""
-        instance.name = validated_data.get('name', instance.name)
-        instance.image = validated_data.get('image', instance.image)
-        instance.save()
+        fields = ('name', 'slug', 'image')
 
 
 class MenuSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Menu.
-
-    Преобразует объекты Menu в JSON и обратно.
-    """
-
     class Meta:
         model = Menu
-        fields = ('name', 'category', 'description', 'image', 'price', 'available', 'popular')
-        # Поля модели Menu, включаемые в сериализацию
-
-    def create(self, validated_data):
-        """Создает новый объект Menu."""
-        menu = Menu.objects.create(**validated_data)
-        return menu
-
-    def update(self, instance, validated_data):
-        """Обновляет существующий объект Menu."""
-        instance.name = validated_data.get('name', instance.name)
-        instance.category = validated_data.get('category', instance.category)
-        instance.description = validated_data.get('description', instance.description)
-        instance.image = validated_data.get('image', instance.image)
-        instance.price = validated_data.get('price', instance.price)
-        instance.available = validated_data.get('available', instance.available)
-        instance.popular = validated_data.get('popular', instance.popular)
-        instance.save()
+        fields = ('id', 'name', 'slug', 'category', 'description', 'image', 'price', 'available', 'popular')
 
 
 class ExtraItemSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели ExtraItem."""
 
     class Meta:
         model = ExtraItem
         fields = ('name', 'price')
 
-    def create(self, validated_data):
-        """Создает новый объект ExtraItem."""
-        extra_item = ExtraItem.objects.create(**validated_data)
-        return extra_item
-
-    def update(self, instance, validated_data):
-        """Обновляет существующий объект ExtraItem."""
-        instance.name = validated_data.get('name', instance.name)
-        instance.price = validated_data.get('price', instance.price)
-        instance.save()
-
-
-class ExtraProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ExtraProduct
-        fields = ('menu', 'extra')
