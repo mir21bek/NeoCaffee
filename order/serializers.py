26023e48b3f72e2678d4fd4658ce90595dd8bb96
mir21bek <mirbek.2121@gmail.com>
2023-12-19 22:13:45 +0600

    merge warehouse

--- order/serializers.py
+++ order/serializers.py
@@ -5,27 +5,27 @@ from .models import Order, OrderItem
 from branches.models import Branches
 
 
-class BranchSerializer(serializers.ModelSerializer):
+class OrderBranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Branches
         fields = ("image", "name")
 
 
-class MenuSerializer(serializers.ModelSerializer):
+class OrderMenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ("image", "name", "price")
 
 
-class ExtraItemSerializer(serializers.ModelSerializer):
+class OrderExtraItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExtraItem
         fields = ("name", "price")
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    menu = MenuSerializer(read_only=True)
-    extra_product = ExtraItemSerializer(read_only=True)
+    menu = OrderMenuSerializer(read_only=True)
+    extra_product = OrderExtraItemSerializer(read_only=True)
     cashback = serializers.SerializerMethodField()
 
     def get_cashback(self, obj):
@@ -45,7 +45,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    branch = BranchSerializer(read_only=True)
+    branch = OrderBranchSerializer(read_only=True)
     items = OrderItemSerializer(many=True)
 
     class Meta:
