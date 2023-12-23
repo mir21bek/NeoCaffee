7006bc1f836ede808e97042606ccdb575ff58954
askalks <139969622+askalks@users.noreply.github.com>
2023-12-24 00:13:53 +0600

    updated the work schedule in the branch serializers and barista serializers

--- administrator/serializers.py
+++ administrator/serializers.py
@@ -51,13 +51,13 @@ class StaffProfileSerializer(serializers.ModelSerializer):
             "last_name",
             "position",
             "branch",
-            "monday",
-            "monday",
-            "wednesday",
-            "thursday",
-            "friday",
-            "saturday",
-            "sunday",
+            "monday", "monday_start_time", "monday_end_time",
+            "tuesday", "tuesday_start_time", "tuesday_end_time",
+            "wednesday", "wednesday_start_time", "wednesday_end_time",
+            "thursday", "thursday_start_time", "thursday_end_time",
+            "friday", "friday_start_time", "friday_end_time",
+            "saturday", "saturday_start_time", "saturday_end_time",
+            "sunday", "sunday_start_time", "sunday_end_time",
         )
 
 
