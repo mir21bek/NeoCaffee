714033da58ed270f40fb4721725bf8fdc96a86ff
mir21bek <mirbek.2121@gmail.com>
2023-12-26 22:36:14 +0600

    rewrited views

--- order/views.py
+++ order/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from administrator.permissions import IsClientUser
+from .models import Order
 from .serializers import OrderSerializer, OrderHistorySerializer
 
 
@@ -17,5 +19,10 @@ class OrderHistory(APIView):
     serializer_class = OrderHistorySerializer
     permission_classes = [IsClientUser]
 
+    def get(self, request, *args, **kwargs):
+        orders = self.get_queryset()
+        serializer = self.serializer_class(orders, many=True)
+        return Response(serializer.data)
+
     def get_queryset(self):
-        return self.request.user
+        return Order.objects.filter(user=self.request.user)
