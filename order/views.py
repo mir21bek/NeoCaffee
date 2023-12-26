557f8f499e8a327a8ce63321efdd8a3df3351f18
mir21bek <mirbek.2121@gmail.com>
2023-12-26 22:27:13 +0600

    addded permissions for order history

--- order/views.py
+++ order/views.py
@@ -1,30 +1,21 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from administrator.permissions import IsClientUser
 from .serializers import OrderSerializer, OrderHistorySerializer
-from .models import Order, OrderItem
 
 
 class OrderCreateAPIView(generics.CreateAPIView):
     serializer_class = OrderSerializer
-    # permission_classes = [IsClientUser]
+    permission_classes = [IsClientUser]
 
-    # def perform_create(self, serializer):
-    #     serializer.save(user=self.request.order.user)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.order.user)
 
 
 class OrderHistory(APIView):
-    def get(self, request, *args, **kwargs):
-        orders = Order.objects.all()
-        serializer = OrderHistorySerializer(orders, many=True)
-        return Response(serializer.data)
+    serializer_class = OrderHistorySerializer
+    permission_classes = [IsClientUser]
 
-    # queryset = OrderItem.objects.all()
-    # serializer_class = OrderSerializer
-    # permission_classes = [IsClientUser]
-
-    # def get_queryset(self):
-    #     user = self.request.user
-    #     return Order.objects.filter(user=user)
+    def get_queryset(self):
+        return self.request.user
