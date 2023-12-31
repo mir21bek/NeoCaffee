6df229eb68c094f9be8e82a922993cfe07353d24
askalks <139969622+askalks@users.noreply.github.com>
2024-01-01 01:35:34 +0600

    Created functionality for writing off ingredients from the warehouse

--- order/signals.py
+++ order/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Order
+from .models import Order, OrderItem
 
 
 @receiver(post_save, sender=Order)
@@ -13,3 +13,12 @@ def order_post_save(sender, instance, created, **kwargs):
         and instance._prev_status != instance.status
     ):
         instance.apply_cashback()
+
+
+@receiver(pre_save, sender=OrderItem)
+def update_stock_on_order(sender, instance, **kwargs):
+    """
+    Обновить запасы на складе при создании заказа.
+    """
+    for ingredient in instance.menu.menu_ingredients.all():
+        ingredient.product.decrease_stock(ingredient.quantity_used * instance.menu_quantity)
