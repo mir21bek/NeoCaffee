1f68b3c6877f0627055fd1a04fbba7290a132789
askalks <139969622+askalks@users.noreply.github.com>
2024-01-01 01:39:16 +0600

    Created functionality for writing off ingredients from the warehouse

--- config/routing.py
+++ config/routing.py
@@ -1,13 +1,13 @@
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.urls import path
-# from order import consumers
-#
-# application = ProtocolTypeRouter(
-#     {
-#         "websocket": URLRouter(
-#             [
-#                 path("ws/some_path/", consumers.NotificationConsumer),
-#             ]
-#         ),
-#     }
-# )
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from order import consumers
+
+application = ProtocolTypeRouter(
+    {
+        "websocket": URLRouter(
+            [
+                path("ws/some_path/", consumers.NotificationConsumer),
+            ]
+        ),
+    }
+)
